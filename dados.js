213b40e91afedd4a5de035c98225ab76ea4219ab
Braitiner <braitiner.andrade@sankhya.com.br>
2024-09-04 13:44:58 -0300

    aula 3, criação de dados

---
+++ dados.js
@@ -0,0 +1,15 @@
+let dados = [
+              {  
+              titulo: "Rayssa Leal",
+              descricao: "Rayssa Leal é uma skatista brasileira, conhecida como 'Fadinha', que se destacou nas Olimpíadas de Tóquio.",
+              link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"},
+              {
+                titulo: "Rebeca Andrade",
+                descricao: "Rebeca Andrade é uma ginasta brasileira, campeã olímpica, mundial e pan-americana.",
+                link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+              },
+              {
+                titulo: "Beatriz Souza",
+                descricao: "Beatriz Souza é uma judoca brasileira que conquistou a medalha de ouro nos Jogos Olímpicos de Paris 2024.",
+                link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+              }];
