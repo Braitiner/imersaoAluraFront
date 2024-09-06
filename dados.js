f321143b9f10c2b53e7fefa358f165f8bbe2f55a
Braitiner <braitiner.andrade@sankhya.com.br>
2024-09-06 14:47:42 -0300

    conclusão curso alura

--- dados.js
+++ dados.js
@@ -2,14 +2,30 @@ let dados = [
               {  
               titulo: "Rayssa Leal",
               descricao: "Rayssa Leal é uma skatista brasileira, conhecida como 'Fadinha', que se destacou nas Olimpíadas de Tóquio.",
-              link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"},
+              link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+              tags: "skate fada sk8"
+              },
               {
                 titulo: "Rebeca Andrade",
-                descricao: "Rebeca Andrade é uma ginasta brasileira, campeã olímpica, mundial e pan-americana.",
-                link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+                descricao: "Rebeca Andrade é uma ginasta brasileira, campeã olímpica, mundial e pan-americana. 8888888888888888",
+                link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+                tags: "ginastica  ginástica ouro sankhya"
+              },
+              {
+                titulo: "Gabriel Medina",
+                descricao: "Gabriel Medina é um dos maiores nomes do surf mundial e um dos atletas brasileiros mais admirados. Medina se destacou desde jovem por seu talento e paixão pelas ondas.",
+                link: "https://monitordomercado.com.br/wp-content/uploads/2024/08/medina.jpeg",
+                tags: "surf  mar prancha"
+              },
+              {
+                titulo: "Gabriel Araujo",
+                descricao: "Gabriel Araújo, também conhecido como Gabrielzinho, é um dos maiores nomes da natação paralímpica brasileira. Nascido com focomelia, uma condição congênita que impede a formação normal de braços e pernas, Gabriel superou todos os desafios e se tornou um exemplo de superação e determinação.",
+                link: "https://pt.wikipedia.org/wiki/Gabrielzinho",
+                tags: "natação  nadador ouro paraolimpiadas"
               },
               {
                 titulo: "Beatriz Souza",
                 descricao: "Beatriz Souza é uma judoca brasileira que conquistou a medalha de ouro nos Jogos Olímpicos de Paris 2024.",
-                link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+                link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+                tags: "judo judô nayara"
               }];
